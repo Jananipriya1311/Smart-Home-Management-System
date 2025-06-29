fda7f5cd8a79e6c9defebfff7d100ccc0b706061
Jananipriya1311 <1311janani@gmail.com>
2025-06-29 19:11:55 +0530

    frontend/src/components directory

--- frontend/src/components/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebaseConfig';
-import { useNavigate } from 'react-router-dom';
-
-export default function Logout() {
-  const navigate = useNavigate();
-  return <button onClick={() => { signOut(auth); navigate('/login'); }}>Logout</button>;
-}
